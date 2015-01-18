d7a6bc63282e77aef7383798e9829365fb2189ba
Mike Hernandez <mhernandez@etsy.com>
2015-01-18 17:45:36 -0500

    oops, end-to-end test will never pass on travis

--- src/test/java/com/beyondnormal/journalslurp/DayOneCliWrapperIntegrationTest.java
+++ src/test/java/com/beyondnormal/journalslurp/DayOneCliWrapperIntegrationTest.java
@@ -7,11 +7,12 @@ import java.io.IOException;
 public class DayOneCliWrapperIntegrationTest {
     @Test
     public void AddJournalEntry() throws DayOneCliBinaryValidatorInvalidBinaryException, InterruptedException, DayOneCliWrapperNoBinarySetException, IOException {
-        DayOneCliWrapper wrapper = new DayOneCliWrapper();
-        wrapper.setPathToDayOne("/usr/local/bin/dayone");
-        StringBuilder entryText = new StringBuilder();
-        entryText.append("I ADDED THIS WITH JAVA");
         // TODO: create a test runner in the IDE or something to avoid having to comment this out
+        // TODO: this is never going to pass in Travis. Sort that out.
+//        DayOneCliWrapper wrapper = new DayOneCliWrapper();
+//        wrapper.setPathToDayOne("/usr/local/bin/dayone");
+//        StringBuilder entryText = new StringBuilder();
+//        entryText.append("I ADDED THIS WITH JAVA");
         //wrapper.addEntry(entryText);
     }
 }
